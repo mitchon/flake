6aac876041ceea7c29e0a024d2745f4e2fc2e841
mitchanx <kommital@yandex.ru>
2026-09-02 00:45:43 +0300

    homelab groups, ip, fw

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -8,32 +8,31 @@
   ];
 
   users = {
-    groups.${user}.gid = 1000;
     users.${user} = {
-      uid = 1000;
-      group = user;
       hashedPassword = "!";
       openssh.authorizedKeys.keyFiles = [
         ../../keys/mitchanx-think-nix.pub
         ../../keys/nugget.pub
       ];
+      extraGroups = [ "video" "render" ];
     };
   };
 
   networking = {
     useDHCP = false;
     interfaces = {
-      eth0.ipv4.addresses = [{
-        address = "192.168.2.53";
-        prefixLength = 24;
-      }];
-      eth1.ipv4.addresses = [{
-        address = "192.168.2.54";
-        prefixLength = 24;
-      }];
+      eth0.ipv4.addresses = [
+        { address = "192.168.2.53"; prefixLength = 24; }
+        { address = "192.168.2.54"; prefixLength = 24; }
+      ];
     };
     defaultGateway = "192.168.2.1";
     nameservers = [ "192.168.2.1" ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 22 ];
+      checkReversePath = false; 
+    };
   };
 
   system.stateVersion = "25.11";
