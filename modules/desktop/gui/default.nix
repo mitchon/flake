cd17c3f2456efce516aa6d7a77f21107dbb98ca8
mitchanx <kommital@yandex.ru>
2026-09-02 00:42:45 +0300

    tlp

--- modules/desktop/gui/default.nix
+++ modules/desktop/gui/default.nix
@@ -11,7 +11,16 @@
     ./utils.nix
   ];
 
-  services.power-profiles-daemon.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT1 = 75;
+      STOP_CHARGE_THRESH_BAT1 = 80; 
+    };
+  };
   services.upower.enable = true;
   services.udisks2.enable = true;
 
